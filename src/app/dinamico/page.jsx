40878b67c1017bc51d9c2fbb80dbc29fe4af4e9c
Arthur <arthurferreira1417@gmail.com>
2024-08-16 19:38:18 -0300

    Feat: adicioando paginas e header

---
+++ src/app/dinamico/page.jsx
@@ -0,0 +1,10 @@
+import * as s from "./styles.jsx";
+import Header from "@/components/header/index.jsx";
+export default function Dinamico() {
+  return (
+    <s.body>
+      <Header />
+      
+    </s.body>
+  );
+}
---
+++ src/components/header/index.jsx
@@ -0,0 +1,9 @@
+import * as s from "./styles.jsx" 
+export default function Header (){
+return(
+    <s.main>
+        <s.op>Botôes Dinamicos</s.op>
+        <s.op></s.op>
+    </s.main>
+)
+}
